3a37ef5bb8d16e3cc17ef65514d23396e6f282b4
shashikant-bhardwaj <bhardwajshashi531@gmail.com>
2026-09-15 09:55:42 +0530

    Refactor prompt creation to use from_template and enhance runnable structure for parallel execution

--- src/rag_project/runnables/parallelrunable.py
+++ src/rag_project/runnables/parallelrunable.py
@@ -3,17 +3,18 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnableLambda
 
 
 
 load_dotenv()
 
 #1. two different prompt
-short_prompt = ChatPromptTemplate.from_messages(
+short_prompt = ChatPromptTemplate.from_template(
     "Explain {topic} in 1-2 lines"
 )
 
-detailed_prompt = ChatPromptTemplate.from_messages(
+detailed_prompt = ChatPromptTemplate.from_template(
     "Explain {topic} in details"
 )
 
@@ -26,3 +27,20 @@ model = ChatGroq(
 
 #3. parser
 parser = StrOutputParser()
+
+#input
+topic = "Machine Learning"
+
+#runnable
+chain = RunnableParallel({
+    "short":RunnableLambda(lambda X : X["short"]) | model | parser
+})
+
+result = chain.invoke({
+    "short": {"topic": "Machine learning"},
+    "detailed": {"topic": "Deep learning"}
+})
+
+print(result["short"])
+print()
+print(result["detailed"])
